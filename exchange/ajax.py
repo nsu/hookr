ed6b10272b261af1a0663a948b84f25832e5ac91
Louis Fogel <lafogel@wpi.edu>
2013-01-03 04:59:00 -0500

    Added full JSON serialization of django models

--- exchange/ajax.py
+++ exchange/ajax.py
@@ -1,16 +1,17 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
+from django.http import HttpResponse
 from django.core import serializers
 from exchange.models import *
-
+from exchange.serializers import *
 @dajaxice_register
 def sayhello(request):
     return simplejson.dumps({'message':'Hello World'})
-
-
+    
 @dajaxice_register(method='GET')
 def get_my_hookups(request):
     my_shares = ShareGroup.objects.filter(owner=request.user)
     hookups = [share.hookup for share in my_shares]
-    data = serializers.serialize("json", hookups)
+    ser=JSONSerializer()
+    data = ser.serialize(hookups, relations='hookers, network')
     return data
