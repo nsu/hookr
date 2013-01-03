0fb4a76c073cfe57add891e3a6b8f2e241f5dcc9
Louis Fogel <lafogel@wpi.edu>
2013-01-03 02:01:14 -0500

    Serialization of hookups now works (incomplete)

--- exchange/ajax.py
+++ exchange/ajax.py
@@ -1,7 +1,7 @@
 from django.utils import simplejson
 from dajaxice.decorators import dajaxice_register
 from django.core import serializers
-from exchange import models
+from exchange.models import *
 
 @dajaxice_register
 def sayhello(request):
@@ -12,6 +12,5 @@ def sayhello(request):
 def get_my_hookups(request):
     my_shares = ShareGroup.objects.filter(owner=request.user)
     hookups = [share.hookup for share in my_shares]
-    #objects = Hookup.objects.filter(id__in=hookup_ids)
     data = serializers.serialize("json", hookups)
     return data
