724047a3a14bdc823ee758ee785f5a5f9d249140
Louis Fogel <lafogel@wpi.edu>
2013-01-04 18:04:31 -0500

    Added placement of sell orders to ajax.py, added matching of buy and sell orders to views.py

--- exchange/ajax.py
+++ exchange/ajax.py
@@ -3,6 +3,7 @@ from dajaxice.decorators import dajaxice_register
 from django.http import HttpResponse
 from django.core import serializers
 from exchange.models import *
+from exchange.views import match_orders
 from exchange.serializers import *
 @dajaxice_register
 def sayhello(request):
@@ -15,3 +16,20 @@ def get_my_hookups(request):
     ser=JSONSerializer()
     data = ser.serialize(hookups, relations='hookers, network')
     return data
+    
+@dajaxice_register
+def place_sell_order(request, volume, price, hookup_pk):
+    hookup = Hookup.objects.get(pk=hookup_pk)
+    #TODO figure out what response objects to use
+    try:
+        ShareGroup.objects.get(owner=request.user)
+        order = SellOrder(owner=request.user, volume=volume, price=price)
+        order.save()
+        buyers = BuyOrder.objects.get(hookup=hookup)
+        for buy_order in buyers:
+            match_orders(buy_order, order)
+                
+    except ShareGroup.DoesNotExist, SellOrder.ValidationError:
+        #User doesn't seem to own enough of these...
+        return False
+    
